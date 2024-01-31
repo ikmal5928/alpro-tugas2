e5b2cbee4c60bf8d1d7eebf4fc3bc5ec2dcedf6f
ikmal5928 <148456816+ikmal5928@users.noreply.github.com>
2024-01-31 21:29:21 +0700

    Add files via upload

---
+++ jawaban.java
@@ -0,0 +1,29 @@
+
+public class Jawaban {
+
+    public static void main(String[] args) {
+        // Tipe data numerik
+        int nilaiInt = 9;
+        long nilaiLong = 4000000L;
+        float nilaiFloat = 2.31f;
+        double nilaiDouble = 2.71828;
+
+        // Tipe data karakter
+        char karakter = 'A';
+
+        // Tipe data boolean
+        boolean nilaiBoolean = true;
+
+        // Tipe data string
+        String teks = "Halo, ini contoh string.";
+
+        // Menampilkan nilai dari tipe data
+        System.out.println("Nilai dari tipe data int: " + nilaiInt);
+        System.out.println("Nilai dari tipe data long: " + nilaiLong);
+        System.out.println("Nilai dari tipe data float: " + nilaiFloat);
+        System.out.println("Nilai dari tipe data double: " + nilaiDouble);
+        System.out.println("Nilai dari tipe data char: " + karakter);
+        System.out.println("Nilai dari tipe data boolean: " + nilaiBoolean);
+        System.out.println("Nilai dari tipe data string: " + teks);
+    }
+}
---
+++ jawaban.py
@@ -0,0 +1,46 @@
+# Tipe data numerik
+integer_number = 11
+float_number = 2.23
+complex_number = 1 + 5j
+
+# Tipe data teks
+string_data = "Hello, World!"
+
+# Tipe data boolean
+boolean_data = True  # atau False
+
+# Tipe data list (daftar)
+list_data = [1, 2, 3, 'a', 'b', 'c']
+
+# Tipe data tuple
+tuple_data = (4, 5, 6, 'x', 'y', 'z')
+
+# Tipe data set (himpunan)
+set_data = {7, 8, 9, 'apple', 'banana', 'cherry'}
+
+# Tipe data dictionary
+dictionary_data = {'name': 'apey', 'age': 21, 'city': 'cobodas'}
+
+# Menampilkan nilai dari masing-masing tipe data
+print("Nilai dari tipe data numerik:")
+print("Integer Number:", integer_number)
+print("Float Number:", float_number)
+print("Complex Number:", complex_number)
+
+print("\nNilai dari tipe data teks:")
+print("String Data:", string_data)
+
+print("\nNilai dari tipe data boolean:")
+print("Boolean Data:", boolean_data)
+
+print("\nNilai dari tipe data list:")
+print("List Data:", list_data)
+
+print("\nNilai dari tipe data tuple:")
+print("Tuple Data:", tuple_data)
+
+print("\nNilai dari tipe data set:")
+print("Set Data:", set_data)
+
+print("\nNilai dari tipe data dictionary:")
+print("Dictionary Data:", dictionary_data)
